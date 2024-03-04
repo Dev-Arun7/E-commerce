3cdba5b7936c8ccc189fdb3bfa36b17b1c68b1d2
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-03-04 19:04:25 +0530

    v7.62 allowed host added

--- gadgetzone_project/settings.py
+++ gadgetzone_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2!8os+qk#fvt4o^)(r_s1*6+p=e&y6-#sf=a*eodi#l&gow6l)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
