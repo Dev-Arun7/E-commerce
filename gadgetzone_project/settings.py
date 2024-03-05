1109a0fb2590f0a19021500ed5aa896142986101
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-03-05 19:17:11 +0530

    v7.65 statis files changed in settings.py

--- gadgetzone_project/settings.py
+++ gadgetzone_project/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+ 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -141,19 +141,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-MEDIA_URL = '/media/'
+# STATIC_URL = '/staticfiles/'
 
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+
+# Define the STATIC_URL
+STATIC_URL = '/static/'
+
+# Define the directory where static files will be collected during deployment
+STATIC_ROOT = BASE_DIR / 'static'
+
+# Define the directories from which to gather static files during development
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "staticfiles",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default# settings.py
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
