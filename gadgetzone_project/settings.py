f962fd56bce884072b30871f20878359084a0d4a
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-03-05 19:22:22 +0530

    v7.66 changed static on settings.py

--- gadgetzone_project/settings.py
+++ gadgetzone_project/settings.py
@@ -141,36 +141,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/staticfiles/'
+STATIC_URL = '/staticfiles/'
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-
-
-# Define the STATIC_URL
-STATIC_URL = '/static/'
-
-# Define the directory where static files will be collected during deployment
-STATIC_ROOT = BASE_DIR / 'static'
-
-# Define the directories from which to gather static files during development
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default# settings.py
-
-
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
